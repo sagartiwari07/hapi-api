4aadc8759bcfef64aabb89ac83dab5175e527ae0
Sagar Tiwari <85619112+sagartiwari07@users.noreply.github.com>
2021-06-09 17:33:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const Hapi = require("hapi");
+
+const server = new Hapi.Server();
+
+server.connection({
+    port:3000,
+    host:'localhost'
+});
+
+server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, response) => {
+        response("Hello World");
+    }
+});
+
+server.start(error => {
+    if(error) {
+        throw error;
+    }
+    console.log("Listening at " + server.info.uri);
+});
